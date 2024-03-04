a8581f0ea7b167b43ff8391d9dd07fc2a8ae18be
Nadine Fiona <nadinefiona@Nadines-MacBook-Pro.local>
2024-03-04 09:37:27 +0200

    updates

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,27 +1,25 @@
 function validateForm() {
+    document.getElementById('nameError').innerText = '';
     document.getElementById('emailError').innerText = '';
     document.getElementById('messageError').innerText = '';
 
-    // Get values from the form
+
+    var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
 
-    // Validate email
+    if (name.length < 5) {
+        document.getElementById('nameError').innerText = 'name must be at least 3 characters.';
+        return false;
+    }
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email) || email.indexOf('@') === -1) {
         document.getElementById('emailError').innerText = 'Please enter a valid email address.';
         return false;
     }
-
-    // Validate message length
     if (message.length < 10) {
         document.getElementById('messageError').innerText = 'Message must be at least 10 characters.';
         return false;
     }
-
-    // If all validations pass, you can submit the form
-    // Uncomment the line below if you want the form to be submitted
-    // document.getElementById('myForm').submit();
-
     return true;
 }
